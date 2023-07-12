eafa3d3e048a2888c4216c50935b4417fc774997
Daniel Beltejar <danielbeltejar@gmail.com>
2023-07-12 22:27:16 +0200

    Added prices history.

---
+++ static/scripts/chart.js
@@ -0,0 +1,74 @@
+function chart(countryName) {
+    const container = document.getElementById("container");
+    container.innerHTML = ""; // Clear the container by removing its child elements
+    
+    const floatingWindow = document.getElementById("floatingWindow");
+    const divEl = floatingWindow.querySelector(".flag");
+    divEl.classList.add("flag-icon", "flag-icon-" + countryName.toLowerCase()); // Set the new text content for the <h2> element
+
+
+    // create line chart
+    var chart = anychart.line();
+
+    // set X axis labels formatter
+    chart
+        .xAxis()
+        .labels()
+        .format(function () {
+            return anychart.format.dateTime(this.value, 'dd/MM');
+        });
+
+    // make an HTTP request to fetch data
+    fetch('http://127.0.0.1:8000/prices/history/' + countryName)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            // create data set on fetched data
+            var dataSet = anychart.data.set(data);
+
+            // map mapping
+            var mapping = dataSet.mapAs({
+                x: 0,
+                value: 1
+            });
+
+            // create spline series
+            var series = chart.spline();
+
+            // set chart data
+            series.data(mapping);
+
+            // set series stroke settings
+            series.stroke({
+                angle: 90,
+                keys: [{
+                    color: '#2fa85a',
+                    offset: 0.25
+                }, {
+                    color: '#ecef17',
+                    offset: 0.5
+                }, {
+                    color: '#ee4237',
+                    offset: 0.75
+                }],
+                thickness: 3
+            });
+
+            // set container id for the chart
+            chart.container('container');
+
+            // initiate chart drawing
+            chart.draw();
+        })
+        .catch(function (error) {
+            console.error('Error:', error);
+        });
+};
+
+document.querySelector(".button-close").addEventListener("click", function () {
+    const closeButton = document.getElementById("closeButton");
+    const floatingWindow = document.getElementById("floatingWindow");
+
+    floatingWindow.style.display = "none"; // Hide the floating window
+});
---
+++ static/scripts/prices.js
@@ -0,0 +1,80 @@
+fetch('https://bigmac.danielbeltejar.es/v1/prices/actual')
+    .then(response => response.json())
+    .then(data => {
+        data.sort((a, b) => a[4] - b[4]);
+        data.forEach(country => {
+            var countryName = country[0];
+            var price = country[1];
+            var currency = country[3];
+            var priceDollars = country[4];
+
+            // Create the card element
+            var card = document.createElement("div");
+            card.classList.add("card", "flex-row", "flex-center", "white-background");
+
+            // Add the flag icon
+            var flagIcon = document.createElement("i");
+            flagIcon.classList.add("flag-icon", "flag-icon-" + countryName.toLowerCase());
+            card.appendChild(flagIcon);
+
+            // Add the price
+            var priceElement = document.createElement("p");
+            priceElement.classList.add("text-bold", "default");
+            priceElement.textContent = price + " " + currency;
+            card.appendChild(priceElement);
+
+            // Add the dollars price
+            var priceDollarsElement = document.createElement("p");
+            priceDollarsElement.classList.add("text-bold", "dollars");
+            priceDollarsElement.textContent = priceDollars + " USD";
+            priceDollarsElement.classList.add("hide")
+            card.appendChild(priceDollarsElement);
+
+
+            // Add the button
+            var button = document.createElement("div");
+            button.classList.add("button", "yellow-background", "white-color");
+            button.id = countryName.toLowerCase
+            button.innerHTML = '<img src="static/images/chart-simple-solid.svg">';
+            button.addEventListener("click", function () {
+                floatingWindow.style.display = "block"; // Show the window
+                chart(countryName);
+            });
+
+
+            card.appendChild(button);
+
+            // Append the card to the index section
+            document.querySelector(".index").appendChild(card);
+        });
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+
+let showingDollars = false;
+
+// Function to convert all prices to dollars
+function convertPricesToDollars() {
+    var priceElements = document.querySelectorAll(".card p.dollars");
+    priceElements.forEach(element => {
+        if (showingDollars) {
+            element.classList.add("hide");
+        } else {
+            element.classList.remove("hide");
+        }
+    });
+    var priceElements = document.querySelectorAll(".card p.default");
+    priceElements.forEach(element => {
+        if (showingDollars) {
+            element.classList.remove("hide");
+        } else {
+            element.classList.add("hide");
+        }
+    });
+    showingDollars = !showingDollars;
+}
+
+// Add click event listener to the button
+document.querySelector(".button-dollars").addEventListener("click", convertPricesToDollars);
+
