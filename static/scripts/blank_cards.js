040738149cf56f48e2e85e05cb161bd8efcf1b63
Daniel Beltejar <danielbeltejar@gmail.com>
2023-08-22 15:12:02 +0200

    Added blank cards while loading.

--- static/scripts/blank_cards.js
+++ static/scripts/blank_cards.js
@@ -1,4 +1,3 @@
-            // Loop to create and append the div 25 times
             for (var i = 0; i < 25; i++) {
                 // Create the card element
                 var card = document.createElement("div");
@@ -11,7 +10,5 @@
                 button.innerHTML = '<img src="static/images/chart-simple-solid.svg">';
                 card.appendChild(button);
 
-                // Append the card to a container (you'll need a container element in your HTML to append the cards)
-                // For example, assuming you have a container with id "cardContainer":
                 document.querySelector(".index").appendChild(card);
             }  
