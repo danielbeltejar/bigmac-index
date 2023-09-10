19178fd2f831c27d1aba14877fded31add1fd0f6
Daniel Beltejar <danielbeltejar@gmail.com>
2023-09-10 17:27:55 +0200

    Ensure cache is always served.

--- server/main.py
+++ server/main.py
@@ -1,10 +1,12 @@
+import asyncio
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi_amis_admin.admin.settings import Settings
 from fastapi_amis_admin.admin.site import AdminSite
 from fastapi_scheduler import SchedulerAdmin
 
-from src.routers.ActualPricesRouter import actual_prices_router
+from src.routers.ActualPricesRouter import actual_prices_router, get_latest_prices_async
 from src.routers.PriceHistoryRouter import price_history_router
 from src.routers.HealthRouter import health_router
 from src.sql.MySQL import MySQL
@@ -27,6 +29,8 @@ app.add_middleware(
 # Create an instance of the scheduled task scheduler `SchedulerAdmin`
 site = AdminSite(settings=Settings(database_url_async='sqlite+aiosqlite:///amisadmin.db'))
 scheduler = SchedulerAdmin.bind(site)
+
+
 mysql = MySQL()
 
 
@@ -39,4 +43,5 @@ async def scrape():
 async def startup():
     # Start the scheduled task scheduler
     scheduler.start()
-    requests.get("http://127.0.0.1:8000/prices/actual")
+    asyncio.create_task(get_latest_prices_async())
+
--- server/src/routers/HealthRouter.py
+++ server/src/routers/HealthRouter.py
@@ -10,9 +10,8 @@ health_router = APIRouter()
 logger = logging.getLogger()
 
 
-@health_router.get('/healthz',
-                   status_code=status.HTTP_200_OK)
-def heath_check():
+@health_router.get('/healthz')
+async def heath_check():
     if len(ActualPricesRouter.cache) != 0:
         return HTTPStatus.OK
     return HTTPStatus.TOO_EARLY
